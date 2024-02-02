ededafb5bef83fc41b9fb3cd4c6a8de1a51e1ca3
rezqyndrizal <rezqy.o@akatrust.com>
2024-02-02 11:00:42 +0700

    Update aside.blade.php

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -86,7 +86,7 @@
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">table_view</i>
                     </div>
-                    <span class="nav-link-text ms-1">Report Lites</span>
+                    <span class="nav-link-text ms-1">Report Liters</span>
                 </a>
             </li>
             <li class="nav-item">
